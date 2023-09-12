f2c36ba45c58d2b3e267ddd3d22f914adbd6ea7b
Evgenii <eugene.pokalyuk@gmail.com>
2023-09-12 20:05:07 +0700

    deploy settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
