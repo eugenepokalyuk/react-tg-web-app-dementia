187503bd87544bc3accc2a85f082ed7b6ed426d3
Evgenii <eugene.pokalyuk@gmail.com>
2023-09-12 19:45:43 +0700

    initial commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+import { useTelegram } from "../../services/hooks/useTelegram";
+import Header from "../Header/Header";
+import { Route, Routes } from 'react-router-dom'
+import ProductList from "../ProductList/ProductList";
+import Form from "../Form/Form";
 
 function App() {
+  const { onToggleButton, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
     </div>
   );
 }
