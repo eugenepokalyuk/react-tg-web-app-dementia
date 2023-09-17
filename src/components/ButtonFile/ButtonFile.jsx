bb4a34ccc6b810424d546265cf18d1c8e223ab19
Evgenii <eugene.pokalyuk@gmail.com>
2023-09-17 16:58:56 +0700

    Do ButtonFile component

---
+++ src/components/ButtonFile/ButtonFile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ButtonFile.module.css'
+
+const ButtonFile = () => {
+    return (
+        <form method="post" encType="multipart/form-data">
+            <label className={`${styles.inputFile}`}>
+                <input type="file" name="file" />
+                <span>Добавить файл</span>
+            </label>
+        </form>
+    )
+};
+export default ButtonFile;
